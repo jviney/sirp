a993623fe86bb95328e04ed759efa4aba22c4ebf
Jonathan Viney <jonathan@kamiapp.com>
2020-11-27 22:04:28 +1300

    Do not require sysrandom/securerandom.

--- lib/sirp.rb
+++ lib/sirp.rb
@@ -1,6 +1,12 @@
 require 'openssl'
 require 'digest'
-require 'sysrandom/securerandom'
+
+# Do not require. Causes errors like:
+# NoMethodError: Failed to open TCP connection to cognito-idp.ap-southeast-2.amazonaws.com:443 (undefined method `<=' for 1024..65535:Range)
+# from .rubies/2.6.6/lib/ruby/2.6.0/net/http.rb:949:in `rescue in block in connect'
+# Caused by NoMethodError: undefined method `<=' for 1024..65535:Range
+# require 'sysrandom/securerandom'
+
 require 'sirp/sirp'
 require 'sirp/parameters'
 require 'sirp/client'
